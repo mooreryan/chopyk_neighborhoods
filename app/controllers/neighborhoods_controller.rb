023561673466c19edeba7abea878a36210c8f63d
Ryan Moore <moorer@udel.edu>
2014-05-05 19:44:30 -0400

    Add dropdown boxes

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -10,10 +10,20 @@ class NeighborhoodsController < ApplicationController
     join_string = 'inner join sequences on interactions.contig = ' + 
       'sequences.header'
 
+    # test the sql
     @requested_contigs = Interaction.select(select_string)
       .joins(join_string)
       .where(interactions: { downstream: params[:down], 
                              upstream: params[:up] })
+
+    # test
+    # @interactions = Interaction.select(:downstream, :upstream)
+    #   .distinct.to_a
+
+    # sort might make it slow with a lot of things
+    @down_names = Interaction.select(:downstream).distinct.to_a.map { |r| [r.downstream, r.downstream] }.sort
+
+    @up_names = Interaction.select(:upstream).distinct.to_a.map { |r| [r.upstream, r.upstream] }.sort
 
     @apple = params
 
